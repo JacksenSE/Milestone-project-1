c51eef4a1ae34fbd693e51429e8378befb926060
JacksenSE <Jacskenbmarshall@gmail.com>
2022-11-04 15:48:27 -0700

    story update

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-var checkIt = confirm("You wake up in the middle of the night, you heard a loud bang downstairs. Check what it was? Or Hide in your room?");
-
-if(checkIt == true) {
-    alert("You walk downstairs to find someone in your house.");
-
-    var downstairsFind = confirm("Do you sneak to the kitchen to grab a weapon or leave through the front door?");
-
-        if (downstairsFind == true){
-            window.prompt("You sneak to the kitchen and slowly open a cabinet. In it you find a Iron Skillet, A Kitchen Knife, and a Banana. Which do you choose? 1 (skillet) 2 (Knife) 3 (Banana)");
-        
-            if(downstairsFind == 1){
-                var chooseSkillet = confirm("You chose the skillet, the robber finds you Do you attack him or defend yourself?");
-                if (chooseSkillet == true){
-                    alert("You chose to attack him he had a gun. You lose, Try again.");
-                }
-                else{
-                    alert("He shoots you, the bullet gets deflected off the pan you win!");
-                }
-            }
-            else if(downstairsFind === 2){
-                
-                var chooseKnife = confirm("You chose the Knife, the robber finds you Do you attack him or defend yourself?");
-                
-                if(chooseKnife === true){
-                    alert("You chose to attack him he punches if you in the face knocking you out cold. You lose, Try again.");
-                }
-                else{
-                    alert("You chose to defend yourself he had a gun. You lose, Try again");
-                }
-            }
-            else (downstairsFind === 3);{
-            var chooseBanana = confirm("You chose the banana the robber is deathly allergic to banana's he explodes. You win! ");
-           
-            }
-        }
-}
-else {
-    alert("You hide in your room and hear footsteps approaching");
-    
-    var hideRoom = confirm("The footsteps go sient outside of your door. Do you hide in bed or go to the door?");
-
-    if (hideRoom == true){
-        alert("You hide in your bed the intruder found you, you lose try again.");
+function start(){
+    let firstAnswer  = window.prompt('"You wake up in the middle of the night, you heard a loud bang downstairs. check? Or hide?')
+    if(firstAnswer === 'check'){
+       cCheck()
+    } else if(firstAnswer === 'hide') {
+        cHide()
     }
-    else{
-        alert("You wait at the door ready to fight the robber you lose, try againfff.");
+}
+
+function cCheck(){
+    let secondAnswer = window.prompt(`You decided to check, you walk down the hallways and see someone in your house. Do you 'sneak' to the kitchen to grab a weapon or 'leave' through the front door?`)
+    if(secondAnswer === 'sneak'){
+        sneak()
+     } else if(secondAnswer === 'leave'){
+         leave()
+     }
+    if(secondAnswer === 'back'){
+        start()
     }
+}
+
+function cHide(){
+    let secondAnswer = window.prompt(`You hide in your room and hear footsteps approaching. The footsteps go sient outside of your door. Do you fight? Or hide`)
+    if(secondAnswer === 'fight'){
+        fight()
+    } else if(secondAnswer === "hide"){   
+        hide()
+    }
+    if(secondAnswer === 'back'){
+        start()
+    }
+}
+
+function sneak(){
+    let thirdAnswer = window.prompt("You sneak to the kitchen and you find a skillet, and a banana. Which do you choose? skillet? Or banana?")
+}if(thirdAnswer ==="skillet"){
+    window.alert("You chose to attack him he had a gun. You lose, Try again.")
+}else if(thirdAnswer ==="banana"){
+    window.alert('You chose the banana the robber is deathly allergic to bananas he explodes. You win!')
+}
+if(thirdAnswer ==='back'){
+    cCheck()
+}
+function leave(){
+let thirdAnswer = window.prompt('You sprint through the front door. The robber is chasing after you. Keep "running"? Or "face" them?')
+if(thirdAnswer === 'running'){
+    window.alert('The robber is faster than you he catches you. You lose.')
+}else if(thirdAnswer === "face"){
+    window.alert('You face the robber and beat him up. You win!')
+}
 
 }
+start()
