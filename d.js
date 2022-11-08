0c040c4fab0b7c95dd23db3f6859d89e58ee1990
JacksenSE <Jacskenbmarshall@gmail.com>
2022-11-07 17:28:07 -0800

    Refactored

--- d.js
+++ d.js
@@ -1,5 +1,5 @@
 const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
+const ChoiceButtonsElement = document.getElementById('Choice-buttons')
 
 let state = {}
 
@@ -9,41 +9,38 @@ function startGame() {
 }
 
 function showTextNode(textNodeIndex) {
-  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  var textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
   textElement.innerText = textNode.text
-  while (optionButtonsElement.firstChild) {
-    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  while (ChoiceButtonsElement.firstChild) {
+    ChoiceButtonsElement.removeChild(ChoiceButtonsElement.firstChild)
   }
 
-  textNode.options.forEach(option => {
-    if (showOption(option)) {
-      const button = document.createElement('button')
-      button.innerText = option.text
+  textNode.Choices.forEach(Choice => {
+    if (showChoice(Choice)) {
+      var button = document.createElement('button') 
+      button.innerText = Choice.text 
       button.classList.add('btn')
-      button.addEventListener('click', () => selectOption(option))
-      optionButtonsElement.appendChild(button)
+      button.addEventListener('click', () => selectChoice(Choice))
+      ChoiceButtonsElement.appendChild(button)
     }
   })
 }
-
-function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
+function showChoice(Choice) {
+  return Choice.requiredState == null || Choice.requiredState(state)
 }
-
-function selectOption(option) {
-  const nextTextNodeId = option.nextText
+function selectChoice(Choice) {
+  var nextTextNodeId = Choice.nextText
   if (nextTextNodeId <= 0) {
     return startGame()
   }
-  state = Object.assign(state, option.setState)
+  state = Object.assign(state, Choice.setState)
   showTextNode(nextTextNodeId)
 }
-
 const textNodes = [
   {
     id: 1,
     text: 'You wake up in the middle of the night, you heard a loud bang in your house. Check it out? or Hide.',
-    options: [
+    Choices: [
       {
         text: 'Check it out',
         nextText: 2
@@ -57,7 +54,7 @@ const textNodes = [
   {
     id: 2,
     text: 'You decided to check, you walk down the hallways and see someone in your house. Do you sneak to the kitchen to grab a weapon or leave through the front door?',
-    options: [
+    Choices: [
       {
         text: 'Leave through front door.',
         nextText: 4
@@ -71,7 +68,7 @@ const textNodes = [
   {
     id: 3,
     text: 'You decided to hide. Time passes by, footsteps approach your door. Fight back or hide under your bed?',
-    options: [
+    Choices: [
       {
         text: 'Hide.',
         nextText: 6
@@ -85,7 +82,7 @@ const textNodes = [
   {
     id: 4,
     text: 'You sprint out of the front door the robber chases behind you. He is faster than you, he catches up to you and idk shoots you lol. ',
-    options: [
+    Choices: [
       {
         text: 'Try again',
         nextText: -1
@@ -95,7 +92,7 @@ const textNodes = [
   {
     id: 5,
     text: 'You sneak into the kitchen on the counter you see a Kitchen knife, a Cast iron skillet, and a Banana. You must choose a weapon to defend yourself',
-    options: [
+    Choices: [
       {
         text: 'Cast iron skillet',
         nextText: 8
@@ -113,7 +110,7 @@ const textNodes = [
   {
     id: 6,
     text: 'You hide under the bed from the robber. He opens the door and walks past you. Luckily he does not check under the bed. He leaves ',
-    options: [
+    Choices: [
       {
         text: 'You cant stay here forever time to get up.',
         nextText: 7
@@ -123,7 +120,7 @@ const textNodes = [
   {
     id: 7,
     text: 'You finally get up now time to come up with a plan. What will you choose?.',
-    options: [
+    Choices: [
       {
         text: 'Try to run?',
         nextText: 4
@@ -145,7 +142,7 @@ const textNodes = [
   {
     id: 8,
     text: 'The robbers close combat skills are better than yours. You lose.',
-    options: [
+    Choices: [
       {
         text: 'Try again',
         nextText: -1
@@ -155,7 +152,7 @@ const textNodes = [
   {
     id: 9,
     text: 'The robber is deathly alergic to bananas he explodes you win!',
-    options: [
+    Choices: [
       {
         text: 'Play again?',
         nextText: -9
@@ -165,7 +162,7 @@ const textNodes = [
   {
     id: 10,
     text: 'You charge your phone. Your phone rings extreamly loud. The robber finds you. You lose.',
-    options: [
+    Choices: [
       {
         text: 'Try again',
         nextText: -1
@@ -175,7 +172,7 @@ const textNodes = [
   {
     id: 11,
     text: 'He accepts your offer you guys have a fun night.',
-    options: [
+    Choices: [
       {
         text: 'You win! Play again?',
         nextText: -9
